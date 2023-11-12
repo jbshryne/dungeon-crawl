d1ce0f280ee1592519cd0975b5927a13062643f5
Jon Brennan <jonbrennan@Jons-MacBook-Pro.local>
2023-11-11 20:20:32 -0500

    implemented battle dice for attacking & defending

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,13 +1,14 @@
-import { INVALID_MOVE } from "boardgame.io/core";
+// import { INVALID_MOVE } from "boardgame.io/core";
 // import { isAdjacentTile } from "./Board";
 
 const hero = {
   name: "Hero",
-  position: 22,
+  position: 9,
   id: 0,
-  attack: 1,
-  hitPoints: 3,
-  team: 0,
+  attackDice: 3,
+  defenseDice: 2,
+  hitPoints: 4,
+  team: "HERO",
   moveTiles: 3,
   hasMoved: false,
   hasDoneAction: false,
@@ -17,9 +18,10 @@ const enemy = {
   name: "Gobbo",
   position: 8,
   id: 1,
-  attack: 1,
+  attackDice: 2,
+  defenseDice: 2,
   hitPoints: 3,
-  team: 1,
+  team: "ENEMY",
   moveTiles: 3,
   hasMoved: false,
   hasDoneAction: false,
@@ -37,43 +39,64 @@ export const DungeonHopper = {
 
   moves: {
     moveOneSquare: ({ G, ctx, events, playerID }, tileIdx) => {
-      // const currentPlayer = G.players[playerID];
-      // const currentPlayerPosition = currentPlayer.position;
-      // const currentPlayerName = currentPlayer.name;
-      // G.tiles[currentPlayerPosition] = null;
-      // G.tiles[tileIdx] = currentPlayerName;
-      // G.players[playerID].position = tileIdx;
-      // currentPlayer.moveTiles -= 1;
-      // currentPlayer.hasMoved = true;
-      // if (currentPlayer.moveTiles === 0) {
-      //   events.endStage();
-      // }
       console.log("You moved to tile " + tileIdx);
       console.log(G);
     },
-    attack: ({ G, playerID }, tileIdx) => {
+    attack: ({ G, playerID, random, events }, tileIdx) => {
       const currentPlayer = G.players[playerID];
+      const currentPlayerName = currentPlayer.name;
       const currentPlayerTeam = currentPlayer.team;
       const attackedPlayer = G.players.find(
         (player) => player.position === tileIdx
       );
-      // console.log(tileIdx, attackedPlayer);
       const attackedPlayerName = attackedPlayer.name;
       const attackedPlayerTeam = attackedPlayer.team;
 
-      if (attackedPlayerTeam && currentPlayerTeam !== attackedPlayerTeam) {
-        attackedPlayer.hitPoints -= currentPlayer.attack;
+      if (
+        attackedPlayerTeam &&
+        currentPlayerTeam !== attackedPlayerTeam &&
+        !currentPlayer.hasDoneAction
+      ) {
+        // events.endStage();
         console.log(
-          `${currentPlayer.name} attacks! ${attackedPlayerName} has ${attackedPlayer.hitPoints} hit points left`
+          currentPlayerName + " attacks " + attackedPlayerName + "..."
         );
+        currentPlayer.hasDoneAction = true;
+        const attackRoll = getBattleDice(random.D6(currentPlayer.attackDice));
+        const defenseRoll = getBattleDice(
+          random.D6(attackedPlayer.defenseDice)
+        );
+        let attackTotal = attackRoll.filter((die) => die === "atk").length;
+        let defenseTotal;
+        if (attackedPlayerTeam === "HERO") {
+          defenseTotal = defenseRoll.filter((die) => die === "hDef").length;
+        } else {
+          defenseTotal = defenseRoll.filter((die) => die === "eDef").length;
+        }
+        // attackedPlayer.hitPoints -= attackTotal - defenseTotal;
+        console.log("Attack roll:", attackRoll, "successes:", attackTotal);
+        console.log("Defense roll:", defenseRoll, "successes:", defenseTotal);
+
+        if (attackTotal - defenseTotal > 0) {
+          attackedPlayer.hitPoints -= attackTotal - defenseTotal;
+          console.log(
+            `SUCCESS! ${attackedPlayerName} has ${attackedPlayer.hitPoints} now hit point(s)`
+          );
+        } else {
+          console.log("Attack misses!");
+        }
+
         if (attackedPlayer.hitPoints <= 0) {
           G.tiles[tileIdx] = null;
           attackedPlayer.position = null;
           console.log(`${attackedPlayerName} is dead!`);
         }
-      } else {
+      } else if (currentPlayerTeam === attackedPlayerTeam) {
         console.log("You can't attack your own team!");
-        return INVALID_MOVE;
+        // return INVALID_MOVE;
+      } else if (currentPlayer.hasDoneAction) {
+        console.log("You have already performed an action this turn!");
+        // return INVALID_MOVE;
       }
     },
     // rollToAttack: ({ G, playerID }, tileIdx) => {
@@ -119,10 +142,20 @@ export const DungeonHopper = {
   },
 
   turn: {
+    onBegin: ({ G }) => {
+      G.players.forEach((player) => {
+        player.hasMoved = false;
+        player.hasDoneAction = false;
+      });
+
+      // console.log(`It is ${currentPlayer.name}'s turn`);
+      // currentPlayer.hasMoved = false;
+      // currentPlayer.hasDoneAction = false;
+    },
     stages: {
       movement: {
         moves: {
-          moveOneSquare: ({ G, ctx, events, playerID }, tileIdx) => {
+          moveOneSquare: ({ G, events, playerID }, tileIdx) => {
             const currentPlayer = G.players[playerID];
             const currentPlayerPosition = currentPlayer.position;
             const currentPlayerName = currentPlayer.name;
@@ -135,10 +168,22 @@ export const DungeonHopper = {
             if (currentPlayer.moveTiles === 0) {
               events.endStage();
             }
-            // console.log("You moved to tile " + tileIdx);
           },
         },
       },
     },
   },
 };
+
+function getBattleDice(diceArray) {
+  const dice = diceArray.map((die) => {
+    if (die === 1) {
+      return "eDef";
+    } else if (die === 2 || die === 3) {
+      return "hDef";
+    } else {
+      return "atk";
+    }
+  });
+  return dice;
+}
